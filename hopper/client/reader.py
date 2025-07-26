144de2d90aeb6408ef33fab7a77ea83ba78bf77e
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-26 15:01:35 +0100

    Improve the JSON reader

--- hopper/client/reader.py
+++ hopper/client/reader.py
@@ -11,28 +11,59 @@ class PipeReader:
 
 
 class JsonReader(PipeReader):
-    def __init__(self, client, pipe_name):
+    def __init__(self, client, pipe_name, read_validator=None):
         super().__init__(client, pipe_name)
+
+        self.read_validator = read_validator if read_validator else self.default_read_validator
+        self.tail = ""
 
         if not self._HOPPER_CLIENT.get_pipe_by_pipe_name(pipe_name).blocking:
             print("WARN: Non-blocking reads may crash the brain!")
 
-    def read(self):
-        s = ""
+    @staticmethod
+    def default_read_validator(_):
+        return True
 
-        j = json.decoder.JSONDecoder()
-
-        while (1):
+    def _try_decode_json(self, s):
+        decoder = json.JSONDecoder()
+        idx = 0
+        while idx < len(s):
+            slice = s[idx:].lstrip()
+            if not slice:
+                break
+            offset = len(s[idx:]) - len(slice)
             try:
-                b = self._HOPPER_CLIENT.read(self._PIPE_NAME, _buf_size=1)
-                s += b.decode("utf-8")
+                obj, end = decoder.raw_decode(slice)
+                if self.read_validator(obj):
+                    idx += offset + end
+                    return obj, s[idx:].rstrip()
+                else:
+                    idx += 1
+            except json.JSONDecodeError as e:
+                idx += 1
+        return None, s
 
-                j.decode(s)
-            except json.decoder.JSONDecodeError:
-                continue
+    def read(self):
+        buffer = self.tail
+        chunk_size = 1024
+        max_buffer_size = 1024 * 1024
+
+        while len(buffer) < max_buffer_size:
+            try:
+                chunk = self._HOPPER_CLIENT.read(
+                    self._PIPE_NAME, _buf_size=chunk_size)
+                if not chunk:
+                    break
+
+                buffer += chunk.decode("utf-8")
+
+                obj, tail = self._try_decode_json(buffer)
+
+                if obj:
+                    self.tail = tail
+                    return obj
+
             except BlockingIOError:
                 continue
 
-            break
-
-        return j.decode(s)
+        return None
