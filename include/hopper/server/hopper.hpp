aab76780d693f9d853a8a2b99fa40f24a310a73a
Nathan Gill <nathan.j.gill@outlook.com>
2026-01-11 16:05:50 +0000

    remove `HandlerType` enum, assign IDs dynamically

--- include/hopper/server/hopper.hpp
+++ include/hopper/server/hopper.hpp
@@ -2,19 +2,21 @@
 #define hopper_hpp_INCLUDED
 
 #include <map>
+#include <string>
 #include <vector>
 
 #include "hopper/server/buffer.hpp"
-#include "hopper/server/handler.hpp"
 #include "hopper/server/pipe.hpp"
 
 namespace hopper {
 
 class HopperServer {
 private:
+    std::map<std::string, int> m_handlers;
+
     std::vector<HopperPipe> m_inputs;
-    std::map<HandlerType, HopperBuffer> m_buffers;
-    std::map<HandlerType, std::vector<HopperPipe>> m_outputs;
+    std::map<int, HopperBuffer> m_buffers;
+    std::map<int, std::vector<HopperPipe>> m_outputs;
 };
 
 }; // namespace hopper
