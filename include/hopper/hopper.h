2a53966b48524dda5892aa477cac8d008023c880
Nathan Gill <nathan.j.gill@outlook.com>
2026-01-15 18:26:57 +0000

    feat: add initial `libhopper` client library

--- include/hopper/hopper.h
+++ include/hopper/hopper.h
@@ -1,6 +1,37 @@
 #ifndef hopper_h_INCLUDED
 #define hopper_h_INCLUDED
 
-void hello();
+#include <unistd.h>
+
+#define HOPPER_IN 1
+#define HOPPER_OUT 2
+#define HOPPER_NONBLOCK 4
+
+/// Structure representing a Hopper pipe
+struct hopper_pipe {
+    const char *name;
+    const char *endpoint;
+    const char *hopper;
+    int fd;
+    int flags;
+};
+
+/// Open a new Hopper pipe specified by `pipe`.
+/// Hopper location will be determined from the `HOPPER_PATH` environment
+/// variable, or can be overridden with the `hopper_pipe.hopper` string.
+/// -1 is returned on error, and errno is set.
+int hopper_open(struct hopper_pipe *pipe);
+
+/// Close a Hopper pipe previously opened by `hopper_open_pipe`.
+void hopper_close(struct hopper_pipe *pipe);
+
+/// Read up to `len` bytes from a Hopper pipe. Value returned indicates
+/// the number of bytes read. -1 is returned on error and errno is set.
+ssize_t hopper_read(struct hopper_pipe *pipe, void *dst, size_t len);
+
+/// Write up to `len` bytes into a Hopper pipe. Value returned indicates
+/// the number of bytes written. -1 is returned on error and errno is set.
+ssize_t hopper_write(struct hopper_pipe *pipe, void *src, size_t len);
 
 #endif // hopper_h_INCLUDED
+
