27d01d1bd1fff9fcb9ff7fa1cd913e9705f57a35
Nathan Gill <nathan.j.gill@outlook.com>
2026-01-24 11:51:49 +0000

    use correct path in `setup.py`

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name="hopper",
     version="0.1.0",
-    packages=["client.py"],
-    package_dir={"hopper": "client.py"},
+    packages=["hopper"],
+    package_dir={"hopper": "client/py"},
     author="Nathan Gill",
     author_email="nathan.j.gill@outlook.com",
 )
