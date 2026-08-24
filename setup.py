94a9edcc3e74876bf7682c088348d6b34e4a0238
Nathan Gill <n@ngill.net>
2026-08-24 22:26:12 +0100

    *: bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hopper",
-    version="0.1.5",
+    version="0.1.6",
     packages=["hopper"],
     package_dir={"hopper": "client/py"},
     author="Nathan Gill",
